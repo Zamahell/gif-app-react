3f191d4e939bff4292a13902f387e05bc2181b67
Zamahell <brau.herrerahuerta@gmail.com>
2023-09-25 02:32:41 -0300

    Design() se agrega archvo css

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+    {/**titulo */}
+    <h1>Gif-app</h1>
+    
+    
+    {/**Input */}
+    
+
+
+    {/**Listado de gif */}
+
+    </>
+  )
+}
